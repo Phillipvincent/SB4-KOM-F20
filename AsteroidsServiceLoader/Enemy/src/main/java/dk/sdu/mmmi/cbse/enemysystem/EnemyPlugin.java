de1324a81223d3fb4d850e33789ec65468ee2183
Phill <phnie19@student.sdu.dk>
2022-04-10 14:17:02 +0200

    Implemented Enemy?

---
+++ AsteroidsServiceLoader/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyPlugin.java
@@ -0,0 +1,52 @@
+package dk.sdu.mmmi.cbse.enemysystem;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class EnemyPlugin implements IGamePluginService {
+
+    private Entity enemy;
+
+    public EnemyPlugin() {
+    }
+
+    @Override
+    public void start(GameData gameData, World world) {
+        System.out.println("butt");
+        
+        // Add entities to the world
+        enemy = createEnemyShip(gameData);
+        world.addEntity(enemy);
+    }
+
+    private Entity createEnemyShip(GameData gameData) {
+        System.out.println("Bigger Butt");
+
+        float deacceleration = 10;
+        float acceleration = 200;
+        float maxSpeed = 300;
+        float rotationSpeed = 5;
+        float x = gameData.getDisplayWidth() / 2;
+        float y = gameData.getDisplayHeight() / 2;
+        float radians = 3.1415f / 2;
+
+        Entity enemy = new Enemy();
+        enemy.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+        enemy.add(new PositionPart(x, y, radians));
+        enemy.add(new LifePart(1,5));
+        
+        return enemy;
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        // Remove entities
+        world.removeEntity(enemy);
+    }
+
+}
